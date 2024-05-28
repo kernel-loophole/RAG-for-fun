c4b97156329f712cc7c946f39f3667b1dca6582d
kernel_loophole <mhani18403@gmail.com>
2024-05-28 12:56:12 +0500

    rag model

---
+++ rag_llama.py
@@ -0,0 +1,20 @@
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from llama_index.llms.ollama import Ollama
+
+documents = SimpleDirectoryReader("data").load_data()
+print(documents)
+# bge-base embedding model
+Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
+
+# ollama
+Settings.llm = Ollama(model="llama3", request_timeout=360.0)
+
+index = VectorStoreIndex.from_documents(
+    documents,
+)
+while True:
+    query_engine = index.as_query_engine()
+    query=input("query:")
+    response = query_engine.query(query)
+    print(response)
--- test_db.py
+++ test_db.py
@@ -1,18 +1,44 @@
-
-import pymongo
-def get_mongo_client(mongo_uri):
-    """Establish connection to the MongoDB."""
-    try:
-        client = pymongo.MongoClient(mongo_uri)
-        print("Connection to MongoDB successful")
-        return client
-    except pymongo.errors.ConnectionFailure as e:
-        print(f"Connection failed: {e}")
-        return None
+import psycopg2
+import sys
 
 
-mongo_uri =""
-if not mongo_uri:
-    print("MONGO_URI not set in environment variables")
+conn_params = {
+ 
+}
 
-mongo_client = get_mongo_client(mongo_uri)
+# Connect to the PostgreSQL database
+try:
+    connection = psycopg2.connect(**conn_params)
+    cursor = connection.cursor()
+
+    # SQL query to fetch IDs from the table
+    sql_query = "SELECT id, details FROM news_dawn;"
+
+    # Execute the SQL query
+    cursor.execute(sql_query)
+
+    # Fetch all rows from the result
+    rows = cursor.fetchall()
+    rows=rows[0:50]
+    # print(rows)
+    # cursor.execute("SELECT * from keywords;")
+    # tables=cursor.fetchall()
+    # print(tables)
+    # # Process each row
+    text_list = []
+    for row in rows:
+        label_list = []
+        label_id = row[0]
+        details = row[1]
+        text_list.append(details)
+
+    
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL:", error)
+
+finally:
+    # Close the cursor and connection
+    if connection:
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed.")
